3b611fabe3d437ce979f802159cfc469bbfba778
jarle pedersen <jarle.pedersen@norconsult.com>
2018-06-14 14:59:58 +0200

    save json locally after preprocessing

--- NinMemApi.DataPreprocessing/Program.cs
+++ NinMemApi.DataPreprocessing/Program.cs
@@ -27,18 +27,17 @@ namespace NinMemApi.DataPreprocessing
 
             IConfigurationRoot configuration = builder.Build();
 
-            string ninConnectionString = configuration.GetConnectionString("Nin");
-            string artsdbconnectionString = configuration.GetConnectionString("artsdbstorage");
-            string urlAlleKoder = configuration["UrlAlleKoder"];
-            string urlVariasjoner = configuration["UrlVariasjon"];
+            var ninConnectionString = configuration.GetConnectionString("Nin");
+            var urlAlleKoder = configuration["UrlAlleKoder"];
+            var urlVariasjoner = configuration["UrlVariasjon"];
 
-            UploadDataToAzure(ninConnectionString, artsdbconnectionString, urlAlleKoder, urlVariasjoner).Wait();
+            StoreData(ninConnectionString, urlAlleKoder, urlVariasjoner).Wait();
 
             Console.WriteLine("Done ...");
             Console.ReadKey();
         }
 
-        private static async Task UploadDataToAzure(string ninConnectionString, string artsdbconnectionString, string urlAlleKoder, string urlVariasjoner)
+        private static async Task StoreData(string ninConnectionString, string urlAlleKoder, string urlVariasjoner)
         {
             var koder = GetKoder(urlAlleKoder, urlVariasjoner);
 
@@ -47,25 +46,23 @@ namespace NinMemApi.DataPreprocessing
             var natureAreaTypes = await NatureAreaTypeLoader.Load(ninConnectionString, Codes.Create(koder.alleKoder));
             var redlistData = await RedlistLoader.Load(ninConnectionString);
             var geographicalData = await GeographicalAreaLoader.Load(ninConnectionString);
-            var taxons = TaxonLoader.Get();
-            var codetree = File.ReadAllText("..\\..\\..\\Data\\kodetre.json");
+            //var taxons = TaxonLoader.Get();
+            //var codetree = File.ReadAllText("..\\..\\..\\Data\\kodetre.json");
             var natureAreaVariables = await NatureAreaVariablesLoader.Load(ninConnectionString);
-            var taxonTraits = File.ReadAllText("..\\..\\..\\Data\\taxonTraits.json");
-
-            var azureStorage = new AzureStorage(new ArtsdbStorageConnectionString { ConnectionString = artsdbconnectionString });
+            //var taxonTraits = File.ReadAllText("..\\..\\..\\Data\\taxonTraits.json");
 
             await Task.WhenAll(
-                    azureStorage.Store(StorageKeys.NatureAreas, natureAreas, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.NatureAreaDescriptionVariables, descriptionVariables, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.NatureAreaTypes, natureAreaTypes, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.NatureAreaRedlistCategories, redlistData.categories, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.NatureAreaRedlistThemes, redlistData.themes, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.NatureAreaGeographicalAreaData, geographicalData, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.Taxons, taxons.Values.ToList(), containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.CodeTree, codetree, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.NatureAreaVariables, natureAreaVariables, containerName: StorageConstants.GraphContainerName),
-                    azureStorage.Store(StorageKeys.TaxonTraits, taxonTraits, containerName: StorageConstants.GraphContainerName)
-                );
+                FileSaver.Store(StorageKeys.NatureAreas, natureAreas),
+                FileSaver.Store(StorageKeys.NatureAreaDescriptionVariables, descriptionVariables),
+                FileSaver.Store(StorageKeys.NatureAreaTypes, natureAreaTypes),
+                FileSaver.Store(StorageKeys.NatureAreaRedlistCategories, redlistData.categories),
+                FileSaver.Store(StorageKeys.NatureAreaRedlistThemes, redlistData.themes),
+                FileSaver.Store(StorageKeys.NatureAreaGeographicalAreaData, geographicalData),
+                //FileSaver.Store(StorageKeys.Taxons, taxons.Values.ToList()),
+                //FileSaver.Store(StorageKeys.CodeTree, codetree),
+                FileSaver.Store(StorageKeys.NatureAreaVariables, natureAreaVariables)
+                //FileSaver.Store(StorageKeys.TaxonTraits, taxonTraits)
+            );
         }
 
         private static (List<KodeInstans> alleKoder, List<KodeInstans> variasjonKoder) GetKoder(string urlAlleKoder, string urlVariasjoner)
@@ -95,6 +92,45 @@ namespace NinMemApi.DataPreprocessing
             }
 
             return kodeinstanser;
+        }
+    }
+
+    internal class FileSaver
+    {
+        private static readonly string CacheFolder = Path.GetTempPath();
+
+        public static async Task Store<T>(string key, T value)
+        {
+            var file = File.Create(GetFileName(key));
+            var fileWriter = new StreamWriter(file);
+
+            var json = typeof(T) == typeof(string) ? value as string : JsonConvert.SerializeObject(value);
+
+            fileWriter.WriteLine(json);
+            fileWriter.Dispose();
+        }
+
+        public async Task<T> Get<T>(string key)
+        {
+            string json = null;
+            var filePath = string.IsNullOrWhiteSpace(CacheFolder) ? null : GetFileName(key);
+
+            if (!string.IsNullOrWhiteSpace(filePath) && File.Exists(filePath))
+            {
+                json = File.ReadAllText(filePath);
+            }
+
+            if (typeof(T) == typeof(string))
+            {
+                return (T)(object)json;
+            }
+
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        private static string GetFileName(string key)
+        {
+            return Path.Combine(CacheFolder, key + ".json");
         }
     }
 }
