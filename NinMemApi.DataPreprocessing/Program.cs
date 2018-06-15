6daff568ae357557ca69f24f9c0d6ba57a511859
jarle pedersen <jarle.pedersen@norconsult.com>
2018-06-15 09:57:25 +0200

    use localStorage also in preprocessing move localStorage to NinMemApi.Data

--- NinMemApi.DataPreprocessing/Program.cs
+++ NinMemApi.DataPreprocessing/Program.cs
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using NinMemApi.Data.Stores.Local;
 
 namespace NinMemApi.DataPreprocessing
 {
@@ -51,17 +52,19 @@ namespace NinMemApi.DataPreprocessing
             var natureAreaVariables = await NatureAreaVariablesLoader.Load(ninConnectionString);
             //var taxonTraits = File.ReadAllText("..\\..\\..\\Data\\taxonTraits.json");
 
+            var localStorage = new LocalStorage();
+
             await Task.WhenAll(
-                FileSaver.Store(StorageKeys.NatureAreas, natureAreas),
-                FileSaver.Store(StorageKeys.NatureAreaDescriptionVariables, descriptionVariables),
-                FileSaver.Store(StorageKeys.NatureAreaTypes, natureAreaTypes),
-                FileSaver.Store(StorageKeys.NatureAreaRedlistCategories, redlistData.categories),
-                FileSaver.Store(StorageKeys.NatureAreaRedlistThemes, redlistData.themes),
-                FileSaver.Store(StorageKeys.NatureAreaGeographicalAreaData, geographicalData),
-                //FileSaver.Store(StorageKeys.Taxons, taxons.Values.ToList()),
-                //FileSaver.Store(StorageKeys.CodeTree, codetree),
-                FileSaver.Store(StorageKeys.NatureAreaVariables, natureAreaVariables)
-                //FileSaver.Store(StorageKeys.TaxonTraits, taxonTraits)
+                localStorage.Store(StorageKeys.NatureAreas, natureAreas),
+                localStorage.Store(StorageKeys.NatureAreaDescriptionVariables, descriptionVariables),
+                localStorage.Store(StorageKeys.NatureAreaTypes, natureAreaTypes),
+                localStorage.Store(StorageKeys.NatureAreaRedlistCategories, redlistData.categories),
+                localStorage.Store(StorageKeys.NatureAreaRedlistThemes, redlistData.themes),
+                localStorage.Store(StorageKeys.NatureAreaGeographicalAreaData, geographicalData),
+                //localStorage.Store(StorageKeys.Taxons, taxons.Values.ToList()),
+                //localStorage.Store(StorageKeys.CodeTree, codetree),
+                localStorage.Store(StorageKeys.NatureAreaVariables, natureAreaVariables)
+                //localStorage.Store(StorageKeys.TaxonTraits, taxonTraits)
             );
         }
 
@@ -92,45 +95,6 @@ namespace NinMemApi.DataPreprocessing
             }
 
             return kodeinstanser;
-        }
-    }
-
-    internal class FileSaver
-    {
-        private static readonly string CacheFolder = Path.GetTempPath();
-
-        public static async Task Store<T>(string key, T value)
-        {
-            var file = File.Create(GetFileName(key));
-            var fileWriter = new StreamWriter(file);
-
-            var json = typeof(T) == typeof(string) ? value as string : JsonConvert.SerializeObject(value);
-
-            fileWriter.WriteLine(json);
-            fileWriter.Dispose();
-        }
-
-        public async Task<T> Get<T>(string key)
-        {
-            string json = null;
-            var filePath = string.IsNullOrWhiteSpace(CacheFolder) ? null : GetFileName(key);
-
-            if (!string.IsNullOrWhiteSpace(filePath) && File.Exists(filePath))
-            {
-                json = File.ReadAllText(filePath);
-            }
-
-            if (typeof(T) == typeof(string))
-            {
-                return (T)(object)json;
-            }
-
-            return JsonConvert.DeserializeObject<T>(json);
-        }
-
-        private static string GetFileName(string key)
-        {
-            return Path.Combine(CacheFolder, key + ".json");
         }
     }
 }
